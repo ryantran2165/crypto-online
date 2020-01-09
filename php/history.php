b9b312b53d8fa5a897082c6d672c6e98fcb0776e
ryan_tran <ryantran2165@gmail.com>
2020-01-08 19:27:38 -0800

    Fix css links and add favicon

--- php/history.php
+++ php/history.php
@@ -35,9 +35,10 @@ function print_html($username)
     echo <<<_END
     <html>
     <head>
+      <link rel="icon" href="../assets/favicon.png">
       <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
       <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-      <link href="css/styles.css" rel="stylesheet">
+      <link href="../css/styles.css" rel="stylesheet">
       <title>Crypto Online - History</title>
     </head>
 
